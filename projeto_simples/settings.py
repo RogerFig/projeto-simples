5bb8f2908f8ab06444c2b0afad835dea2cd87eba
rfigsousa@gmail.com <rfigsousa@gmail.com>
2018-01-16 18:53:57 -0300

    Allowed hosts

--- projeto_simples/settings.py
+++ projeto_simples/settings.py
@@ -77,13 +77,15 @@ WSGI_APPLICATION = 'projeto_simples.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 db_from_env = dj_database_url.config(conn_max_age=500)
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 DATABASES[default].update(db_from_env)
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+
 
 
 # Password validation
